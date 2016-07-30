271b58055a991d6d1f3d20c606badf988ea558a1
harlan <hello@harlankellaway.com>
2016-07-30 12:49:00 -0400

    Created first pod file and installed on Demo

--- StylishStringDemo/Pods/Target Support Files/Pods-StylishStringDemo/Pods-StylishStringDemo-frameworks.sh
+++ StylishStringDemo/Pods/Target Support Files/Pods-StylishStringDemo/Pods-StylishStringDemo-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-StylishStringDemo/StylishString.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-StylishStringDemo/StylishString.framework"
+fi
