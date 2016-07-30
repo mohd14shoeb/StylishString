33d34287e7b1c8d6e9e45d8cb01566270fac8607
harlan <hello@harlankellaway.com>
2016-07-30 00:40:34 -0400

    Created tvOS target

---
+++ StylishString-tvOS/StylishString-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  StylishString-tvOS.h
+//  StylishString-tvOS
+//
+//  Created by Harlan Kellaway on 7/30/16.
+//  Copyright © 2016 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StylishString-tvOS.
+FOUNDATION_EXPORT double StylishString_tvOSVersionNumber;
+
+//! Project version string for StylishString-tvOS.
+FOUNDATION_EXPORT const unsigned char StylishString_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StylishString_tvOS/PublicHeader.h>
+
+
